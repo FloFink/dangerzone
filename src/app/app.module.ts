4f5eaeb45eceed741633dba1eb4df91a0a6a4141
Florian Fink <florian.fink@prodyna.com>
2021-04-28 15:51:35 +0200

    added card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { TripDetailsComponent } from './trip-details/trip-details.component';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card'; 
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
